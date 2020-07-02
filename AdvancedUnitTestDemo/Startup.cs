30b1dbf487e334ef75c6c607276369c383e3f6a5
ChrisTorng <christorng@hotmail.com>
2020-07-02 16:49:43 +0800

    Add Microsoft.EntityFrameworkCore.SqlServer package. appsettings.json adds ConnectionStrings. appsettings.Development.json adds EntityFrameworkCore log. Startup.cs deletes ExceptionHandler. Adds Model\Student.cs.

--- AdvancedUnitTestDemo/Startup.cs
+++ AdvancedUnitTestDemo/Startup.cs
@@ -1,4 +1,4 @@
-using Microsoft.AspNetCore.Builder;
+﻿using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
@@ -30,8 +30,6 @@ namespace AdvancedUnitTestDemo
             }
             else
             {
-                app.UseExceptionHandler("/Home/Error");
-
                 // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
                 app.UseHsts();
             }

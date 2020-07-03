4f8188b18764918d1e9ec4c547d0690f8ea3c22e
ChrisTorng <christorng@hotmail.com>
2020-07-03 15:23:37 +0800

    HomeController.Index adds all logic. Index.cshtml adds content.

--- AdvancedUnitTestDemo/Controllers/HomeController.cs
+++ AdvancedUnitTestDemo/Controllers/HomeController.cs
@@ -1,4 +1,7 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using System.Linq;
+using AdvancedUnitTestDemo.Model;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Logging;
 
 namespace AdvancedUnitTestDemo.Controllers
@@ -12,11 +15,40 @@ namespace AdvancedUnitTestDemo.Controllers
             this.logger = logger;
         }
 
-        public IActionResult Index()
+        public IActionResult Index(string sortOrder, string searchString)
         {
-            this.logger.LogInformation($"{nameof(HomeController)}.{nameof(this.Index)}()");
+            this.logger.LogInformation($"{nameof(HomeController)}.{nameof(this.Index)}({sortOrder},{searchString})");
 
-            return this.View();
+            this.ViewData["NameSortParm"] =
+                string.IsNullOrEmpty(sortOrder) ? "name_desc" : string.Empty;
+
+            this.ViewData["DateSortParm"] =
+                sortOrder == "date" ? "date_desc" : "date";
+
+            this.ViewData["SearchString"] = searchString;
+
+            using var studentContext = new SchoolContext();
+
+            var students = studentContext.Students;
+            IQueryable<Student> searchedStudents = students;
+
+            if (!string.IsNullOrEmpty(searchString))
+            {
+#pragma warning disable CA1307 // Specify StringComparison
+                searchedStudents = students.Where(s => s.LastName.Contains(searchString)
+                                       || s.FirstMidName.Contains(searchString));
+#pragma warning restore CA1307 // Specify StringComparison
+            }
+
+            searchedStudents = sortOrder switch
+            {
+                "name_desc" => searchedStudents.OrderByDescending(s => s.LastName),
+                "date" => searchedStudents.OrderBy(s => s.EnrollmentDate),
+                "date_desc" => searchedStudents.OrderByDescending(s => s.EnrollmentDate),
+                _ => searchedStudents.OrderBy(s => s.LastName),
+            };
+
+            return this.View(nameof(this.Index), searchedStudents.AsNoTracking().ToArray());
         }
     }
 }

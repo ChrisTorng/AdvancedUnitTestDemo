1fe40fc47d6e1dbf323765122a89c4843bcc73cc
ChrisTorng <christorng@hotmail.com>
2020-07-17 13:48:57 +0800

    3. InMemorySqlite

---
+++ SchoolDatabaseTests/InMemorySqliteSchoolContext.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Common;
+using System.Linq;
+using Microsoft.Data.Sqlite;
+using Microsoft.EntityFrameworkCore;
+
+namespace SchoolDatabase.Tests
+{
+    internal class InMemorySqliteSchoolContext : ISchoolDatabase
+    {
+        private readonly DbConnection connection;
+        private readonly SchoolContext schoolContext;
+        private bool disposed = false;
+
+        public IQueryable<Student> Students =>
+            this.schoolContext.Students;
+
+        private static DbConnection CreateInMemoryDatabase()
+        {
+            var connection = new SqliteConnection("Filename=:memory:");
+
+            connection.Open();
+
+            return connection;
+        }
+
+        public InMemorySqliteSchoolContext(IEnumerable<Student>? students = null)
+        {
+            this.connection = CreateInMemoryDatabase();
+            this.schoolContext = new SchoolContext(new DbContextOptionsBuilder<SchoolContext>()
+                .UseSqlite(this.connection)
+                .Options);
+
+            this.schoolContext.Database.EnsureCreated();
+
+            if (students != null)
+            {
+                this.schoolContext.Students.AddRange(students);
+                this.schoolContext.SaveChanges();
+            }
+        }
+
+        protected virtual void Dispose(bool disposing)
+        {
+            if (this.disposed)
+            {
+                return;
+            }
+
+            if (disposing)
+            {
+                this.schoolContext.Dispose();
+                this.connection.Dispose();
+            }
+
+            this.disposed = true;
+        }
+
+        public void Dispose()
+        {
+            this.Dispose(true);
+            GC.SuppressFinalize(this);
+        }
+    }
+}
---
+++ SchoolDatabaseTests/InMemorySqliteStudentRepositoryTests.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Linq;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace SchoolDatabase.Tests
+{
+    [TestClass]
+    public class InMemorySqliteStudentRepositoryTests
+    {
+        [TestMethod]
+        public void StudentRepository_EmptyAllStudents_Test()
+        {
+            using var schoolContext = new InMemorySqliteSchoolContext(Array.Empty<Student>());
+            var studentRepository = new StudentRepository(schoolContext);
+
+            Assert.AreEqual(0, studentRepository.AllStudents.ToArray().Length);
+        }
+
+        [TestMethod]
+        public void StudentRepository_AllStudentsQuery_Test()
+        {
+            using var schoolContext = new InMemorySqliteSchoolContext(new Student[]
+                {
+                    new Student
+                    {
+                        FirstMidName = "a",
+                        LastName = "b",
+                        EnrollmentDate = new DateTime(2, 2, 2),
+                    },
+                    new Student
+                    {
+                        FirstMidName = "c",
+                        LastName = "d",
+                        EnrollmentDate = new DateTime(3, 3, 3),
+                    },
+                });
+
+            var studentRepository = new StudentRepository(schoolContext);
+            Assert.IsTrue(studentRepository.AllStudents.Any());
+            Assert.AreEqual(2, studentRepository.AllStudents.Count());
+
+            var student = studentRepository.AllStudents
+                .Where(s => s.FirstMidName == "c").Single();
+            Assert.AreEqual("d", student.LastName);
+            Assert.AreEqual(new DateTime(3, 3, 3), student.EnrollmentDate);
+        }
+    }
+}

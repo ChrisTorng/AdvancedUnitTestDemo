01c8940946902b2d98471445fb2af3918d69b4bd
ChrisTorng <christorng@hotmail.com>
2020-07-17 14:19:44 +0800

    4. InMemoryDatabase

---
+++ SchoolDatabaseTests/InMemoryDatabaseSchoolContext.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.EntityFrameworkCore;
+
+namespace SchoolDatabase.Tests
+{
+    internal class InMemoryDatabaseSchoolContext : ISchoolDatabase
+    {
+        private readonly SchoolContext schoolContext;
+        private bool disposed = false;
+
+        public IQueryable<Student> Students =>
+            this.schoolContext.Students;
+
+        public InMemoryDatabaseSchoolContext(IEnumerable<Student>? students = null)
+        {
+            this.schoolContext = new SchoolContext(new DbContextOptionsBuilder<SchoolContext>()
+                .UseInMemoryDatabase(Guid.NewGuid().ToString())
+                .Options);
+
+            this.schoolContext.Database.EnsureCreated();
+
+            if (students != null)
+            {
+                this.schoolContext.Students.AddRange(students);
+                this.schoolContext.SaveChanges();
+            }
+        }
+
+        protected virtual void Dispose(bool disposing)
+        {
+            if (this.disposed)
+            {
+                return;
+            }
+
+            if (disposing)
+            {
+                this.schoolContext.Dispose();
+            }
+
+            this.disposed = true;
+        }
+
+        public void Dispose()
+        {
+            this.Dispose(true);
+            GC.SuppressFinalize(this);
+        }
+    }
+}

40d021b2ead271de5e02597d296b3b6aa74be883
ChrisTorng <christorng@hotmail.com>
2020-07-17 14:19:56 +0800

    5. Mock

---
+++ SchoolDatabaseTests/MockSchoolContext.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+namespace SchoolDatabase.Tests
+{
+#pragma warning disable CA1063 // Implement IDisposable Correctly
+    public class MockSchoolContext : ISchoolDatabase
+#pragma warning restore CA1063 // Implement IDisposable Correctly
+    {
+        public MockSchoolContext(IEnumerable<Student> students)
+        {
+            this.Students = students.AsQueryable();
+        }
+
+        public IQueryable<Student> Students { get; }
+
+#pragma warning disable CA1063 // Implement IDisposable Correctly
+#pragma warning disable CA1816 // Dispose methods should call SuppressFinalize
+        public void Dispose()
+#pragma warning restore CA1816 // Dispose methods should call SuppressFinalize
+#pragma warning restore CA1063 // Implement IDisposable Correctly
+        {
+        }
+    }
+}
